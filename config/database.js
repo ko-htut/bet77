572d204dd83f8abb12cd518f32929c7a24bcafed
Ko Htut <kohtut@192.168.1.53>
2023-01-21 07:09:53 +0700

    fix:node

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 
 module.exports = {
-	'url': 'mongodb://127.0.0.1:27017',
+	'url': 'mongodb://Kohtut:<Asdfasdf>@ac-3sisltm-shard-00-00.xrx3nv3.mongodb.net:27017,ac-3sisltm-shard-00-01.xrx3nv3.mongodb.net:27017,ac-3sisltm-shard-00-02.xrx3nv3.mongodb.net:27017/?ssl=true&replicaSet=atlas-12af4p-shard-0&authSource=admin&retryWrites=true&w=majority',
 	'options': {
 		//'user':   '00',
 		//'pass':   '000',
